9b528a6abdd6ac2ada83a05ade99db22daee10d6
Rajin (Laptop) <rajin>
2020-02-21 10:29:39 -1000

    Formatting updates for tiles and slides

---
+++ scripts/SlideView.js
@@ -0,0 +1,47 @@
+let slideDelay = 3
+let manualOverrideDelay = 5
+
+var currentIndex
+var advanceIntervalHandle = 0
+var resetIntervalHandle = 0
+
+function getSlideToggleCallback(index) {
+    return () => {
+        if (advanceIntervalHandle != null) {
+            clearInterval(advanceIntervalHandle)
+            advanceIntervalHandle = null
+        }
+
+        if (resetIntervalHandle != null) {
+            clearTimeout(resetIntervalHandle)
+        }
+
+        currentIndex = index
+
+        resetIntervalHandle = setTimeout(resetAdvanceInterval, manualOverrideDelay * 1000)
+
+        // console.log("toggle callback")
+    }
+}
+
+let slideToggles = document.getElementsByClassName("slide-toggle")
+for (var i = 0; i < slideToggles.length; i++) {
+    let toggle = slideToggles[i]
+    toggle.addEventListener("change", getSlideToggleCallback(i))
+}
+
+function advanceSlide() {
+    if (slideToggles.length > 1) {
+        currentIndex = (currentIndex + 1) % slideToggles.length
+        document.getElementById(`slide${currentIndex}`).checked = true
+    
+        // console.log(`advance slide to ${currentIndex}`)
+    }
+}
+function resetAdvanceInterval() {
+    advanceIntervalHandle = setInterval(advanceSlide, slideDelay * 1000)
+
+    // console.log("reset advance interval")
+}
+
+currentIndex = slideToggles.length - 1
--- scripts/TileGrid.js
+++ scripts/TileGrid.js
@@ -1,20 +1,3 @@
-// Handle video hover thumbnails
-
-// let videos = document.getElementsByTagName("video")
-// for (var i = 0; i < videos.length; i++) {
-//     let index = i;
-
-//     videos[index].parentElement.parentElement.onmouseenter = () => {
-//         videos[index].play();
-//         // console.log(`start video for ${videos[index].parentElement.parentElement.id}`)
-//     }
-
-//     videos[index].parentElement.parentElement.onmouseleave = () => {
-//         videos[index].pause();
-//         // console.log(`pause video for ${videos[index].parentElement.parentElement.id}`)
-//     }
-// }
-
 let hoverThumbs = document.querySelectorAll("[tile-hover-thumb]")
 for (var i = 0; i < hoverThumbs.length; i++) {
     let thumb = hoverThumbs[i]
